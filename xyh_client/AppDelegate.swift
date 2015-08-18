1a5fb24f01ea8b10d52659f3c18cf3d1c21b9b16
ken-gu <ken@xiangyouhui.cn>
2015-08-18 16:42:10 +0800

    Local Notification

--- xyh_client/AppDelegate.swift
+++ xyh_client/AppDelegate.swift
@@ -20,18 +20,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         pageController.pageIndicatorTintColor = UIColor.lightGrayColor()
         pageController.currentPageIndicatorTintColor = UIColor.blackColor()
         pageController.backgroundColor = UIColor.whiteColor()
-        NSThread.sleepForTimeInterval(2)
+        
+        let ver:String = UIDevice.currentDevice().systemVersion
+        if (ver as NSString).floatValue >= 8.0{
+            application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound | UIUserNotificationType.Alert | UIUserNotificationType.Badge , categories: nil))
+        }
+        
+        //NSThread.sleepForTimeInterval(2)
         return true
+    }
+    
+    //接收本地通知
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+        let alert = UIAlertView(title: "通知", message: notification.alertBody, delegate: nil, cancelButtonTitle: "知道了")
+        alert.show()
     }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        //var notification = UIApplication.sharedApplication().
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        //进入后台时添加本地通知... （测试功能）
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        let notification = UILocalNotification()
+        var date:NSDate = NSDate()
+        notification.fireDate = date.dateByAddingTimeInterval(3.0)
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        notification.alertBody = "这是一个本地通知"
+        notification.alertAction = "知道了"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.applicationIconBadgeNumber = 1;
+        
+        var aUserInfo:NSMutableDictionary = NSMutableDictionary()
+        aUserInfo["kLocalNotificationID"] = "anID"
+        notification.userInfo = aUserInfo as [NSObject : AnyObject]
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
